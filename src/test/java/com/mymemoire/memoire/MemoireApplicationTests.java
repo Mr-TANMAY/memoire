05690cd0aeead91974b24186bbd1701cbf0ebb57
Mr.Tanmay <tanmaykadam8828@gmail.com>
2024-12-26 16:27:11 +0530

    Initial commit

---
+++ src/test/java/com/mymemoire/memoire/MemoireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mymemoire.memoire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
